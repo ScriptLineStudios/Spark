97fc369759ffce548b71f591b1c004b8e1c704c4
ScriptLineStudios <scriptlinestudios@protonmail.com>
2022-05-07 11:30:57 +0200

    Added basic triangle rendering

--- examples/helloworld.py
+++ examples/helloworld.py
@@ -3,4 +3,6 @@ import spark
 spark.init("Hello World This is a test", 1200, 600) 
 
 while spark.not_window_close():
-    spark.render()
+    spark.clear()
+    spark.render_triangle()
+    spark.update()
--- src/spark.c
+++ src/spark.c
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 
 GLFWwindow* window;
+#define GLSL(src) "#version 150 core\n" #src
 
 static PyObject* version(PyObject* self){
     return Py_BuildValue("s", "Version 1.0");
@@ -49,6 +50,8 @@ static PyObject* should_window_close(PyObject* self){
 }
 
 static PyObject* render(PyObject* self){
+
+
     glfwSwapBuffers(window);
     glfwPollEvents();
     Py_INCREF(Py_None);
@@ -56,11 +59,85 @@ static PyObject* render(PyObject* self){
     return Py_None;
 }
 
+static PyObject* clear_screen(PyObject* self){
+    //TODO: Add color argument
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    Py_INCREF(Py_None);
+    
+    return Py_None;
+}
+
+static PyObject* render_triangle(PyObject* self){
+    // Create Vertex Array Object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    float vertices[] = {
+         0.0f,  0.5f, // Vertex 1 (X, Y)
+         0.5f, -0.5f, // Vertex 2 (X, Y)
+        -0.5f, -0.5f  // Vertex 3 (X, Y)
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    const char* vertexSource = GLSL(
+        in vec2 position;
+        
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    );
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    const char* fragmentSource = GLSL(
+        out vec4 outColor;
+        
+        void main() {
+            outColor = vec4(0.0f, 1.0f, 1.0f, 1.0f);
+        }
+    );
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    Py_INCREF(Py_None);
+    return Py_None; 
+}
+
 static PyMethodDef myMethods[] = {
     {"init", (PyCFunction)init, METH_VARARGS, "Inits"},
     {"version", (PyCFunction)version, METH_NOARGS, "Returns version"},
     {"not_window_close", (PyCFunction)should_window_close, METH_NOARGS, "Close"},
-    {"render", (PyCFunction)render, METH_NOARGS, "Render"},
+    {"render_triangle", (PyCFunction)render_triangle, METH_NOARGS, "Render Triangle"},
+    {"clear", (PyCFunction)clear_screen, METH_NOARGS, "Clears Screen"},
+    {"update", (PyCFunction)render, METH_NOARGS, "Render"},
 
     {NULL, NULL, 0, NULL}
 };
