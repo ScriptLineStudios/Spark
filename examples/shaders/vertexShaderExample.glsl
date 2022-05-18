8a9b64586c5c9788b69b4e3fe9bb1c25cfc1c94a
ScriptLineStudios <scriptlinestudios@protonmail.com>
2022-05-18 13:43:18 +0200

    Forgot new folder

---
+++ examples/shaders/vertexShaderExample.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 aColor;
+layout (location = 2) in vec2 aTex;
+
+out vec3 outColor;
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
+    outColor = aColor;
+	texCoord = aTex;
+}
+
